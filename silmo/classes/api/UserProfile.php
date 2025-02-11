fb1406dbaecbb4001b24b44afdbef536e7aeda67
silmo-yokohama <silmo.yokohama@gmail.com>
2025-02-11 20:53:08 +0900

    スキル追加

--- silmo/classes/api/UserProfile.php
+++ silmo/classes/api/UserProfile.php
@@ -16,6 +16,17 @@ class UserProfile extends ApiController
     'business_contents',
     'histories',
   ];
+
+  function __construct()
+  {
+    try {
+      $this->data = self::getData();
+    } catch (Exception $e) {
+      $this->hasError = true;
+      $this->errorMessage = $e->getMessage();
+    }
+  }
+
   /**
    * WordPressのカスタムフィールドからユーザープロファイル情報を取得し、
    * キーをキャメルケースに変換して返却します
@@ -29,17 +40,59 @@ class UserProfile extends ApiController
       $values[$key] = get_field($key, "option");
     }
 
+    $values['skills'] = self::getSkills();
+
     // キーをキャメルケースに変換
     return KeyConverter::toCamelCase($values);
   }
 
-  function __construct()
+
+  public static function getSkills()
   {
-    try {
-      $this->data = self::getData();
-    } catch (Exception $e) {
-      $this->hasError = true;
-      $this->errorMessage = $e->getMessage();
+    $terms = get_terms([
+      'taxonomy' => 'skills',
+      'hide_empty' => false,
+    ]);
+
+    // 階層構造を維持するための再帰関数
+    function buildHierarchy($terms, $parentId = 0)
+    {
+      $branch = [];
+      foreach ($terms as $term) {
+        if ($term->parent == $parentId) {
+          $children = buildHierarchy($terms, $term->term_id);
+
+          $termData = [
+            'id' => $term->term_id,
+            'name' => $term->name,
+            'slug' => $term->slug,
+          ];
+
+          // 子要素が存在する場合は追加
+          if (!empty($children)) {
+            $termData['children'] = $children;
+          }
+
+          // レベル情報の追加（親要素以外の場合）
+          if ($parentId !== 0) {
+            $termData['level'] = get_field('level', 'skills_' . $term->term_id);
+          }
+
+          $branch[] = $termData;
+        }
+      }
+
+      // idの昇順でソート
+      if (!empty($branch)) {
+        usort($branch, function ($a, $b) {
+          return $a['id'] - $b['id'];
+        });
+      }
+
+      return $branch;
     }
+
+    // 階層構造を構築
+    return buildHierarchy($terms);
   }
 }
--- silmo/index.php
+++ silmo/index.php
@@ -2,4 +2,4 @@
 require_once __DIR__ . '/classes/api/UserProfile.php';
 
 // プロファイル情報を取得してJSONとして出力
-echo json_encode(UserProfile::getData());
+debug(UserProfile::getData());
